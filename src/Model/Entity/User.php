e54d6966af0bae7ef6adf9b42db2c4293800c1c6
Tony <ihsi0002@student.monash.edu>
2025-03-13 22:12:43 +1100

    Add _setPassword method to hash user passwords securely

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -54,4 +55,19 @@ class User extends Entity
     protected array $_hidden = [
         'password',
     ];
+
+    /**
+     * _setPassword method
+     *
+     * @param string $password Password
+     * @return string|null
+     */
+    protected function _setPassword(string $password): ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+
+        return null;
+    }
 }
