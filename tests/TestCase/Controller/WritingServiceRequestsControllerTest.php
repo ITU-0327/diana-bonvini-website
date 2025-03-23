c4ad10e74457bb59b5227727394f0e97807a2bbf
ganglin <glin0011@student.monash.edu>
2025-03-23 12:58:05 +1100

    Setup WritingServiceRequestsControllerTest.php

---
+++ tests/TestCase/Controller/WritingServiceRequestsControllerTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\WritingServiceRequestsController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\WritingServiceRequestsController Test Case
+ *
+ * @uses \App\Controller\WritingServiceRequestsController
+ */
+class WritingServiceRequestsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var list<string>
+     */
+    protected array $fixtures = [
+        'app.WritingServiceRequests',
+        'app.Users',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::index()
+     */
+    public function testIndex(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::view()
+     */
+    public function testView(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::add()
+     */
+    public function testAdd(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::edit()
+     */
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::delete()
+     */
+    public function testDelete(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test info method
+     *
+     * @return void
+     * @uses \App\Controller\WritingServiceRequestsController::info()
+     */
+    public function testInfo(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
