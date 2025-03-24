4715fc420a1fe17a0ecabdf87d69e1285338f2bd
ganglin <glin0011@student.monash.edu>
2025-03-24 15:33:05 +1100

    update correct details from database in WritingServiceRequestsFixture.php, test index and add in WritingServiceRequestsControllerTest.php

--- tests/TestCase/Controller/WritingServiceRequestsControllerTest.php
+++ tests/TestCase/Controller/WritingServiceRequestsControllerTest.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Test\TestCase\Controller;
 
-use App\Controller\WritingServiceRequestsController;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
@@ -34,7 +33,26 @@ class WritingServiceRequestsControllerTest extends TestCase
      */
     public function testIndex(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+
+        $this->session([
+            'Auth' => [
+                'user_id' => '17fe31f7-2f61-4176-a036-172eed559e6f',
+                'email' => 'tony.hsieh@example.com',
+            ],
+        ]);
+
+        $this->get('/writing-service-requests');
+
+        $this->assertResponseOk();
+
+        $this->assertResponseContains('Writing Service Requests');
+
+        $this->assertResponseContains('creative_writing');
+
+        $this->assertTemplate('index');
+
+        $this->assertResponseContains('test.pdf');
     }
 
     /**
@@ -45,7 +63,25 @@ class WritingServiceRequestsControllerTest extends TestCase
      */
     public function testView(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+
+        $this->session([
+            'Auth' => [
+                'user_id' => '17fe31f7-2f61-4176-a036-172eed559e6f',
+                'email' => 'tony.hsieh@example.com',
+            ],
+        ]);
+
+        $requestId = 'ea42d088-4396-46d7-92d1-4e60c59b2ef7';
+
+        $this->get('/writing-service-requests/view/' . $requestId);
+
+        $this->assertResponseOk();
+
+        $this->assertResponseContains('creative_writing');
+        $this->assertResponseContains('under_5000');
+
+        $this->assertTemplate('view');
     }
 
     /**
