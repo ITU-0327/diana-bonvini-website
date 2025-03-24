3f4d18d5b044af21ff95f41c0686b183404d9b45
ganglin <glin0011@student.monash.edu>
2025-03-24 15:33:05 +1100

    update correct details from database in WritingServiceRequestsFixture.php, test index and add in WritingServiceRequestsControllerTest.php

--- tests/Fixture/WritingServiceRequestsFixture.php
+++ tests/Fixture/WritingServiceRequestsFixture.php
@@ -5,33 +5,27 @@ namespace App\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
-/**
- * WritingServiceRequestsFixture
- */
 class WritingServiceRequestsFixture extends TestFixture
 {
-    /**
-     * Init method
-     *
-     * @return void
-     */
     public function init(): void
     {
         $this->records = [
             [
                 'request_id' => 'ea42d088-4396-46d7-92d1-4e60c59b2ef7',
-                'user_id' => 'acaf89c5-f7a8-4ee3-b92e-77cd1462159e',
-                'service_type' => 'Lorem ipsum dolor sit amet',
-                'word_count_range' => 'Lorem ipsum dolor sit amet',
-                'notes' => 'Lorem ipsum dolor sit amet',
-                'estimated_price' => 1.5,
-                'final_price' => 1.5,
-                'request_status' => 'Lorem ipsum dolor sit amet',
-                'is_deleted' => 1,
+                'user_id' => '17fe31f7-2f61-4176-a036-172eed559e6f',
+                'service_type' => 'creative_writing',
+                'word_count_range' => 'under_5000',
+                'notes' => 'Test writing service request for Tony.',
+                'estimated_price' => 100.00,
+                'final_price' => 150.00,
+                'request_status' => 'pending',
+                'is_deleted' => 0,
                 'created_at' => '2025-03-17 12:54:09',
                 'updated_at' => '2025-03-17 12:54:09',
+                'document' => 'uploads/documents/test.pdf',
             ],
         ];
+
         parent::init();
     }
 }
