1f4f581b773c2ac84c7ec0937d76ec051f79cd56
Tony <ihsi0002@student.monash.edu>
2025-03-25 16:25:07 +1100

    Remove unused add method from OrdersController

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -172,27 +172,6 @@ class OrdersController extends AppController
     }
 
     /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $order = $this->Orders->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $order = $this->Orders->patchEntity($order, $this->request->getData());
-            if ($this->Orders->save($order)) {
-                $this->Flash->success(__('The order has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The order could not be saved. Please, try again.'));
-        }
-        $users = $this->Orders->Users->find('list', limit: 200)->all();
-        $this->set(compact('order', 'users'));
-    }
-
-    /**
      * Edit method
      *
      * @param string|null $id Order id.
