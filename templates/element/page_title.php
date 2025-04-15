24457d7c63c39f2998e9bc5e0b592c01fcdd2f6b
Tony <ihsi0002@student.monash.edu>
2025-04-16 06:40:40 +1000

    Refactor page headers to use a consistent element for title rendering

---
+++ templates/element/page_title.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @var string $title the title of the page
+ */
+?>
+<div class="flex flex-col items-start mb-8">
+    <h1 class="text-3xl uppercase text-gray-800"><?= h($title) ?></h1>
+    <div class="mt-1 w-16 h-[2px] bg-gray-800"></div>
+</div>
